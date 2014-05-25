b860172d3aeb98043675dc2c2dc63fdf79a71433
Maksym Naboka <maksym.naboka@deutschetelekomhbs.com>
2014-05-24 17:24:04 -0700

    CLI test

--- src/main/groovy/com/dthbs/FlowSpecMain.groovy
+++ src/main/groovy/com/dthbs/FlowSpecMain.groovy
@@ -1,10 +1,23 @@
 package com.dthbs
 
+import groovy.util.CliBuilder;
+import org.apache.commons.cli.ParseException;
+
 
 public class FlowSpecMain {
 	public FlowSpecMain() {
-		println "Generating class"
-		ClassGenerator cg = new ClassGenerator("MyNewClass")
+
+	}
+	
+	public static void main(String[] args) {
+		CliBuilder cli = new CliBuilder(usage: 'ls');
+		cli.dsl(args:1, argName:'DslScript', 'Path to Build Flow DSL script');
+		cli.h("Help")
+		def options = cli.parse(args)
+		//println options.dsl
 		
+		if(options.h || options.arguments().size() < 1) {
+			cli.usage();
+		}
 	}
 }
